1317cc9f92dfba911d80100ca264341ff8c3ba2d
Ricardo Freire <rika57@hotmail.fr>
2015-11-26 21:28:57 +0100

    Implementation basique de la classe Card

---
+++ project/Card.scala
@@ -0,0 +1,8 @@
+class Card(fleches : Array[Int], atq : Int, defPhysique : Int, defMagique : Int, type : String){
+	var tabFleches : Array[Int] = fleches
+	var attaque : Int = atq
+	var dPhys : Int = defPhysique
+	var dMag : Int = defMagique
+	var type : String = String
+	
+}
