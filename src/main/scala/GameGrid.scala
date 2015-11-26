1161e7f3d773bf501fd9eacc249b0e3bc5c79d65
WardenQQ <matthieu.michels@etu.unistra.fr>
2015-11-26 22:46:32 +0100

    Ajout de la fonctionalité d'ajout de bloc aléatoire(symbolisé par des 1).

--- src/main/scala/GameGrid.scala
+++ src/main/scala/GameGrid.scala
@@ -1,5 +1,22 @@
+import scala.util.Random
+import Array._
+
 class GameGrid {
+  var grid = ofDim[Int](GameGrid.width, GameGrid.height)
+
+  val numBlocks = Random.nextInt(GameGrid.maxBlocks)
+  for (i <- 0 to numBlocks)
+    grid(Random.nextInt(GameGrid.width))(Random.nextInt(GameGrid.height)) = 1
+
+  override def toString: String = {
+    grid.map(l => l.foldLeft("")((a, b) => a.concat(b.toString)))
+      .foldLeft("")((a, b) => a.concat(b.toString).concat("\n"))
+  }
+
+}
+
+object GameGrid {
   val width = 4
   val height = 4
-  var grid:Array[Int] = new Array[Int](width * height)
+  val maxBlocks = 6
 }
