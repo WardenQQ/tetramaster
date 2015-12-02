23dff25aebeee60d0dc289d70eab73a2cab3682e
xouved <devrouxyann@gmail.com>
2015-12-03 00:49:20 +0100

    Ajout geoloc

--- TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/MainActivity.java
+++ TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/MainActivity.java
@@ -1,7 +1,8 @@
 package fr.u_strasbg.tetramaster.tetramasterclientandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     String addr;
     int port;
     Button btn_envois;
+    Button btn_geoloc;
     EditText txt_connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         port = 1024;
 
         btn_envois     = (Button) findViewById(R.id.btn_envois);
+        btn_geoloc     = (Button) findViewById(R.id.btn_geoloc);
         txt_connect    = (EditText) findViewById(R.id.txt_connect);
 
         btn_envois.setOnClickListener(new OnClickListener(){
@@ -27,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View arg0){
                         Client clt = new Client(addr, port, txt_connect);
                         clt.execute();
+            }
+        });
+        btn_geoloc.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Geolocalisation.class));
             }
         });
     }
