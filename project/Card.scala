64492d1e19b2fcfeff89dca9f3d6bb3f83d3fdb6
WardenQQ <matthieu.michels@etu.unistra.fr>
2015-12-02 18:59:25 +0100

    Finition du merge la branche master.

--- project/Card.scala
+++
@@ -1,3 +0,0 @@
-class Card(val fleches : Array[Int], val atq : Int, val defPhysique : Int, val defMagique : Int, val type : String){
-	/** COUCOU JE SUIS EN PLS */
-}
---
+++ src/main/scala/Card.scala
@@ -0,0 +1,3 @@
+class Card(val fleches : Array[Int], val atq : Int, val defPhysique : Int, val defMagique : Int, val atqType : String){
+	/** COUCOU JE SUIS EN PLS */
+}
