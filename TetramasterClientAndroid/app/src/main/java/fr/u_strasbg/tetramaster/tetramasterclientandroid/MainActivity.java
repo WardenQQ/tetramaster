7e1ba55ec7e7a32a1717f021ae395a6c3cdaf60d
Ricardo Freire <rika57@hotmail.fr>
2016-01-18 21:24:29 +0100

    Login fb et google

--- TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/MainActivity.java
+++ TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/MainActivity.java
@@ -1,25 +1,95 @@
 package fr.u_strasbg.tetramaster.tetramasterclientandroid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    String addr;
-    int port;
+import android.widget.Toast;
+import com.facebook.*;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.fitness.data.Session;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+
+public class MainActivity extends AppCompatActivity  {
+    /*String addr;
+    int port;*/
+    private static final int RC_SIGN_IN = 9001;
+    CallbackManager callbackManager;
     Button btn_connect;
+    LoginButton loginButton;
     EditText txt_connect;
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
+    GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .enableAutoManage(this /* FragmentActivity */, null /* OnConnectionFailedListener */)
+    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+    .build();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        addr = "192.168.0.134";//"130.79.206.217";
-        port = 1024;
+        /*addr = "192.168.0.134";//"130.79.206.217";
+        port = 1024;*/
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+        signInButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                switch (v.getId()) {
+                                                    case R.id.sign_in_button:
+                                                        signIn();
+                                                        break;
+
+                                                }
+                                            }
+                                        });
+
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button_fb);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "LOG VIA FB OK", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), Connected.class));
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "LOG VIA FB CANCEL", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getApplicationContext(), "LOG VIA FB FAIIIIIIL", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btn_connect      = (Button) findViewById(R.id.connectButton);
 
@@ -30,7 +100,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Connected.class));
             }
         });
-
-
+    }
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 }
