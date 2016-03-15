020096ae56a392d1bb8ce05484ff22932ac951e6
Steut57 <rika57@hotmail.fr>
2016-03-15 15:12:52 +0100

    Avancee sur myCollection

--- TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/CardViewFullScreen.java
+++ TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/CardViewFullScreen.java
@@ -1,9 +1,11 @@
 package fr.u_strasbg.tetramaster.tetramasterclientandroid;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.graphics.*;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 import fr.u_strasbg.tetramaster.shared.Card;
 
@@ -16,22 +18,24 @@ public class CardViewFullScreen extends View {
         super(context);
         myCard=card;
         Log.d(TAG, "Im here :");
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        int x=300;
-        int y=300;
+        int x=400;
+        int y=400;
         Paint paintCard = new Paint();
         paintCard.setStyle(Paint.Style.FILL);
         canvas.drawRect(0, 0, x, y, paintCard);
-        int def = myCard.getMagicalDef();
-        int atq = myCard.getAttack();
+        String atqHex=Integer.toHexString(myCard.getAttack()).toUpperCase();
+        String physDefHex=Integer.toHexString(myCard.getPhysicalDef()).toUpperCase();
+        String magDefHex=Integer.toHexString(myCard.getMagicalDef()).toUpperCase();
+        String type = myCard.getPowerType().toUpperCase();
+        String attributes = atqHex+type+physDefHex+magDefHex;
         Paint paintText = new Paint();
         paintText.setColor(Color.YELLOW);
         paintText.setTextSize(16);
-        canvas.drawText(String.valueOf(atq),70,y/2,paintText);
-        canvas.drawText(String.valueOf(def),30,y/2,paintText);
+        canvas.drawText(attributes,x/2-30,y-20,paintText);
     }
 }
--- TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/MyCollection.java
+++ TetramasterClientAndroid/app/src/main/java/fr/u_strasbg/tetramaster/tetramasterclientandroid/MyCollection.java
@@ -1,26 +1,52 @@
 package fr.u_strasbg.tetramaster.tetramasterclientandroid;
 
 import android.app.Application;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
 import fr.u_strasbg.tetramaster.shared.Card;
 
 public class MyCollection extends AppCompatActivity {
     ListAdapter lAdapter;
     ListView list;
     Card[] collection;
+    PopupWindow popUp;
+    Button btn_return, btn_newdeck;
+    boolean clicked=false;
     boolean[] arrows = {true,false,true,false,true,false,true,false};
+    private static final String TAG = "MyDebug";
     Card card = new Card(arrows,2,3,4,"Magic");
+    Card card2 = new Card(arrows,5,1,7,"Physical");
+    Card card3 = new Card(arrows,10,1,12,"Toto");
+    Card card4 = new Card(arrows,1,13,14,"TOTO");
+    private final int popUpWidth = 400;
+    private final int popUpHeight = 400;
+    private int windowWidth, windowHeight;
+    LinearLayout par;
+
+
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mycollection);
+        btn_return = (Button) findViewById(R.id.btn_return);
+        btn_newdeck = (Button) findViewById(R.id.btn_newdeck);
         list = (ListView) findViewById(R.id.list_cards);
         lAdapter=new ListAdapter(this);
         list.setAdapter(lAdapter);
+        Point size = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        windowWidth = size.x;
+        windowHeight = size.y;
         collection = new Card[20];
         collection[0]=card;
         collection[1]=card;
@@ -32,23 +58,45 @@ public class MyCollection extends AppCompatActivity {
         collection[7]=card;
         collection[8]=card;
         collection[9]=card;
-        collection[10]=card;
-        collection[11]=card;
-        collection[12]=card;
-        collection[13]=card;
-        collection[14]=card;
-        collection[15]=card;
-        collection[16]=card;
-        collection[17]=card;
-        collection[18]=card;
-        collection[19]=card;
+        collection[10]=card2;
+        collection[11]=card2;
+        collection[12]=card2;
+        collection[13]=card2;
+        collection[14]=card2;
+        collection[15]=card3;
+        collection[16]=card4;
+        collection[17]=card2;
+        collection[18]=card2;
+        collection[19]=card2;
         lAdapter.setDeck(collection);
         lAdapter.setTeam(0);
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                new CardViewFullScreen(getApplicationContext(),collection[position]);
+                popUp=new PopupWindow(new CardViewFullScreen(getApplicationContext(),collection[position]),popUpWidth,popUpHeight);
+                popUp.setOutsideTouchable(true);
+                popUp.setTouchable(true);
+                popUp.setBackgroundDrawable(new ColorDrawable());
+                if(!clicked)
+                {
+                    popUp.showAtLocation(v, Gravity.TOP, windowWidth/2-popUpWidth/2, windowHeight/2-popUpHeight/2);
+                    clicked=true;
+                }
+                else
+                {
+                    popUp.dismiss();
+                    clicked=false;
+                }
+                //popUp.update(300,300);
+            }
+        });
+        btn_return.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Connected.class));
             }
         });
     }
